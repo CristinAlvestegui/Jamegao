33fa8c3717bddfad9b2e6fda4edb9f7da072c7fa
Cristina Alvéstegui <104037804+CristinAlvestegui@users.noreply.github.com>
2022-07-13 19:04:58 -0300

    Update SQLeditado.sql

--- Documentação/SQLeditado.sql
+++ Documentação/SQLeditado.sql
@@ -2,6 +2,7 @@ create database Jamegao;
 use Jamegao;
 
 create table Jamegoes(
+cod int not null auto_increment primary key,
 jameg varchar(15) not null primary key
 )Engine=InnoDB;
 
@@ -9,6 +10,7 @@ alter table Jamegoes add atalho varchar(20) not null;
 describe Jamegoes;
 
 create table Atalho(
+cod int not null auto_increment primary key,
 atal varchar(25) not null primary key
 ) Engine = InnoDB;
 
